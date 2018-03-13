88cde8131df13ba0609d7eb0b9505a8f26e27192
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-13 13:38:51 -0400

    create a new post

--- m5server/routes/post-routes.js
+++ m5server/routes/post-routes.js
@@ -10,7 +10,6 @@ const postUploader = multer({
 	dest: __dirname + '/../public/uploads/'
 });
 
-
 // create a new post under car 
 postRoutes.post('/api/mcars/new/post', postUploader.single('postPhoto'), (req, res, next) => {
 	if (!req.user) {
@@ -25,7 +24,27 @@ postRoutes.post('/api/mcars/new/post', postUploader.single('postPhoto'), (req, r
 		date: req.body.date,
 		discussion: req.body.discussion
 	});
-})
+	 if(req.file){
+        newPost.image = '/uploads' + req.file.filename;
+    }
+    newPost.save((err) => {
+        if(err){
+            res.status(500).json({message: "Error occurred from the database."});
+            return;
+        }
+        // validation errors
+        if (err && newPhone.errors){
+            res.status(400).json({
+                postingError: newPost.errors.brand,
+            });
+            return;
+        }
+        req.user.encryptedPassword = undefined;
+        newPost.user = req.user;
+
+        res.status(200).json(newPost);
+    });
+});
 
 
 module.exports = postRoutes;
