64664c82e582d6bbd18956b06de2cdb0df2cf1d7
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-13 19:51:37 -0400

    routing and angular materialize configuration

--- m5Client/src/app/app.module.ts
+++ m5Client/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterializeModule } from "angular2-materialize";
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ m5Client/src/app/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  template: `
+	<nav>
+    <div class="nav-wrapper blue lighten-1">
+      <a style="margin-left:10px;" href="#" class="brand-logo">M5Power</a>
+      <ul id="nav-mobile" class="right">
+        <li><a href="/login">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+  `,
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
