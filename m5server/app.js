efb1979ecfe542ddeb3ecd9a8b8c739a978d1cfa
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-13 13:04:31 -0400

    app routing

--- m5server/app.js
+++ m5server/app.js
@@ -54,25 +54,17 @@ app.use("/auth", authRoutes);
 var mcarRoutes = require("./routes/mcars-routes");
 app.use("/", mcarRoutes);
 
+var postRoutes = require("./routes/post-routes");
+app.use("/", postRoutes);
+
 // ==============================================
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res, next) => {
+  // If no routes match, send user to Angular HTML page.
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
---
+++ m5server/routes/post-routes.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const multer = require('multer');
+const postRoutes = express.Router();
+
+
+
+module.exports = postRoutes;
