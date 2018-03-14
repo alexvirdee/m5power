32d9233a140f334419d9482dd3d8745d56db1d1e
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-14 10:21:29 -0400

    routing

--- m5Client/src/app/app.module.ts
+++ m5Client/src/app/app.module.ts
@@ -2,30 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterializeModule } from "angular2-materialize";
 
-// routing
-import { RouterModule } from '@angular/router';
-// import { routes } from './app.routing';
+
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingComponent } from './landing/landing.component';
-import { SearchComponent } from './search/search.component';
-import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LandingComponent,
-    SearchComponent,
-    LoginComponent
+    LandingComponent
   ],
   imports: [
     BrowserModule,
     MaterializeModule
-    // RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
