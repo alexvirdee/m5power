085da9474b8405d814c9aa08eaf76337a2fa5f9c
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-14 10:49:43 -0400

    search and login routing

--- m5Client/src/app/app.module.ts
+++ m5Client/src/app/app.module.ts
@@ -2,23 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterializeModule } from "angular2-materialize";
 
-
+// routing
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+
+
+const routes: Routes = [
+  { path: 'signup',  component: LandingComponent },
+  { path: 'login',  component: LoginComponent },
+  { path: 'search', component: SearchComponent }
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LandingComponent
+    LandingComponent,
+    SearchComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- m5Client/src/app/navbar/navbar.component.ts
+++ m5Client/src/app/navbar/navbar.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
     <div class="nav-wrapper blue lighten-1">
       <a style="margin-left:10px;" href="#" class="brand-logo left">M5Power</a>
       <ul id="nav-mobile" class="right">
-      	<li><a href="/mcars">Search</a></li>
-        <li><a href="/login">Login</a></li>
+      	<li><a [routerLink]="['search']">Search</a></li>
+        <li><a [routerLink]="['login']">Login</a></li>
       </ul>
     </div>
   </nav>
