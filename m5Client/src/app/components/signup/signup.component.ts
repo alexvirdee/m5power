4702a15285c501e73904b2a77e36712a7ec5ce9c
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-15 09:59:11 -0400

    add signup feature

--- m5Client/src/app/components/signup/signup.component.ts
+++ m5Client/src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+signUpInfo = {
+  	username: "",
+  	password: ""
+  };
+
+  errorMessage: string;
+
+  constructor( private myAuth: AuthService, private myRouter: Router ) { }
 
   ngOnInit() {
   }
+
+  doSignUp() {
+  	this.myAuth
+  	.signup(this.signUpInfo)
+  	.then(resultFromApi => {
+  		// clear form
+  		this.signUpInfo = { username: "", password: ""};
+
+  		// clear error message
+  		this.errorMessage = "";
+
+  		// redirect to phones
+  		this.myRouter.navigate(["/search"]);
+  	})
+  	.catch(err => {
+  		const parsedError = err.json();
+  		this.errorMessage = parsedError.message + " 😤";
+  	});
+  } // close doSignUp()
+}
 
 }
