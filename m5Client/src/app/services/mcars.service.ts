0d35a4809fc9b225cd40f23e364b88a5c191baf3
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-16 13:50:37 -0400

    setup mcars service

--- m5Client/src/app/services/mcars.service.ts
+++ m5Client/src/app/services/mcars.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { environment } from '../../environments/environment';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class McarsService {
