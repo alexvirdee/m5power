c52cc57a118eb827b5c2bdcc4935ad07e9e9f474
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-19 09:01:39 -0400

    new post component imports

--- m5Client/src/app/components/new-post/new-post.component.ts
+++ m5Client/src/app/components/new-post/new-post.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { McarsService } from '../../services/mcars.service';
+import { FileUploader } from 'ng2-file-upload';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
+
 
 @Component({
   selector: 'app-new-post',
@@ -6,6 +12,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent implements OnInit {
+  postData = {
+  	
+  }
 
   constructor() { }
 
