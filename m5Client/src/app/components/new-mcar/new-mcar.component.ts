b6e3e6f5df3fb512ed8aab1103a16064a71ccbcc
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-19 20:38:18 -0400

    save mcar to database

--- m5Client/src/app/components/new-mcar/new-mcar.component.ts
+++ m5Client/src/app/components/new-mcar/new-mcar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { McarsService } from "../../services/mcars.service";
 import { AuthService } from '../../services/auth.service';
+import { FileUploader } from 'ng2-file-upload';
 import { environment } from "../../../environments/environment";
 
 @Component({
@@ -19,7 +20,7 @@ export class NewMcarComponent implements OnInit {
 
  savingErr: string
 
- mcarUploader = new Fileuploader({
+ mcarUploader = new FileUploader({
  	url: environment.apiBase + "/api/mcars/new",
  	itemAlias: "mcarImage"
  });
@@ -27,6 +28,32 @@ export class NewMcarComponent implements OnInit {
   constructor( private myRouter: Router, private myCarService: McarsService, private myAuth: AuthService ) { }
 
   ngOnInit() {
+  	this.myAuth
+  		.checklogin()
+  		// on success the user is logged in
+  		.then()
+  		// catch to avoid a console error
+  		.catch(err => {
+  			console.log(err);
+  			this.myRouter.navigate(["/"]);
+  		});
+  }
+
+  saveNewMcar() {
+  	if (this.mcarUploader.getNotUploadedItems(). length === 0) {
+  		this.saveMCarNoImage();
+  	} else {
+  		this.saveMCarWithImage();
+  	}
+  }
+
+  saveMCarNoImage() {
+
+  }
+
+
+  saveMCarWithImage() {
+  	
   }
 
 
