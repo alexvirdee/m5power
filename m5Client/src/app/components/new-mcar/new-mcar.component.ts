2df0d2cb439b71e8c0c694192d397b8d1f2fafa7
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-19 20:34:39 -0400

    add new mcar

--- m5Client/src/app/components/new-mcar/new-mcar.component.ts
+++ m5Client/src/app/components/new-mcar/new-mcar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { McarsService } from "../../services/mcars.service";
+import { AuthService } from '../../services/auth.service';
 import { environment } from "../../../environments/environment";
 
 @Component({
@@ -10,7 +11,20 @@ import { environment } from "../../../environments/environment";
 })
 export class NewMcarComponent implements OnInit {
 
-  constructor(private myRouter: Router, private myCarService: McarsService) { }
+ mcarData = {
+ 	modelM: "",
+ 	year: "",
+ 	specs: ""
+ }
+
+ savingErr: string
+
+ mcarUploader = new Fileuploader({
+ 	url: environment.apiBase + "/api/mcars/new",
+ 	itemAlias: "mcarImage"
+ });
+
+  constructor( private myRouter: Router, private myCarService: McarsService, private myAuth: AuthService ) { }
 
   ngOnInit() {
   }
